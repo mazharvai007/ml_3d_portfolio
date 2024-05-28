d36f76db5ba0662fc252cf16b58bf601a9e25499
mazhar <swopnil420@gmail.com>
2024-05-28 23:06:09 +0600

    Completed the initial setup with routing

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from './Navbar';
+import Alert from './Alert';
+import CTA from './CTA';
+import Footer from './Footer';
+import HomeInfo from './HomeInfo';
+import Loader from './Loader';
+
+export { Navbar, Alert, CTA, Footer, HomeInfo, Loader };
